176767aaf44fd43e1030939bd829f1c9201ec4d9
darkoverlordofdata <darkoverlordofdata@gmail.com>
2017-10-15 14:07:43 -0700

    sync linked libs

--- symlinks.sh
+++ symlinks.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env sh
 
-ln -s /home/bruce/Applications/zerog/src/goop goop
-ln -s /home/bruce/Applications/zerog/src/entitas entitas
-ln -s /home/bruce/Applications/zerog/src/sdx sdx
-ln -s /home/bruce/Applications/zerog/src/vapis vapis
-ln -s /home/bruce/Applications/zerog/src/vapis-em vapis-em
-ln -s /home/bruce/Applications/zerog/src/mt19937ar.c mt19937ar.c
-ln -s /home/bruce/Applications/zerog/src/mt19937ar.h mt19937ar.h
+ln -s /home/bruce/Applications/zerog/lib/goop src/goop
+ln -s /home/bruce/Applications/zerog/lib/entitas src/entitas
+ln -s /home/bruce/Applications/zerog/lib/sdx src/sdx
+ln -s /home/bruce/Applications/zerog/lib/vapis src/vapis
+ln -s /home/bruce/Applications/zerog/lib/vapis-em src/vapis-em
+ln -s /home/bruce/Applications/zerog/lib/mt19937ar.c src/mt19937ar.c
+ln -s /home/bruce/Applications/zerog/lib/mt19937ar.h src/mt19937ar.h
 
