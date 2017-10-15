646ce1eb71cb8315a73d85a46363acb2e1a4872e
Bruce Davidson <darkoverlordofdata@gmail.com>
2017-10-15 13:58:38 -0700

    sync

---
+++ symlinks.bat
@@ -0,0 +1,8 @@
+
+mklink /D C:\Users\darko\Documents\GitHub\zerog-shmupwarz\src\goop C:\msys64\mingw64\share\zerog\lib\goop 
+mklink /D C:\Users\darko\Documents\GitHub\zerog-shmupwarz\src\entitas C:\msys64\mingw64\share\zerog\lib\entitas
+mklink /D C:\Users\darko\Documents\GitHub\zerog-shmupwarz\src\sdx C:\msys64\mingw64\share\zerog\lib\sdx
+mklink /D C:\Users\darko\Documents\GitHub\zerog-shmupwarz\src\vapis C:\msys64\mingw64\share\zerog\lib\vapis
+mklink /D C:\Users\darko\Documents\GitHub\zerog-shmupwarz\src\vapis-em C:\msys64\mingw64\share\zerog\lib\vapis-em
+mklink C:\Users\darko\Documents\GitHub\zerog-shmupwarz\src\mt19937ar.c C:\msys64\mingw64\share\zerog\lib\mt19937ar.c
+mklink C:\Users\darko\Documents\GitHub\zerog-shmupwarz\src\mt19937ar.h C:\msys64\mingw64\share\zerog\lib\mt19937ar.h
---
+++ symlinks.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env sh
+
+ln -s /home/bruce/Applications/zerog/src/goop goop
+ln -s /home/bruce/Applications/zerog/src/entitas entitas
+ln -s /home/bruce/Applications/zerog/src/sdx sdx
+ln -s /home/bruce/Applications/zerog/src/vapis vapis
+ln -s /home/bruce/Applications/zerog/src/vapis-em vapis-em
+ln -s /home/bruce/Applications/zerog/src/mt19937ar.c mt19937ar.c
+ln -s /home/bruce/Applications/zerog/src/mt19937ar.h mt19937ar.h
+
