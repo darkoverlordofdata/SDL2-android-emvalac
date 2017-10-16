be80c84853b59d94185ecda44aba8f2f90fa56d6
Bruce Davidson <darkoverlordofdata@gmail.com>
2017-10-15 18:05:21 -0700

    updates for windows10

--- symlinks.bat
+++ symlinks.bat
@@ -1,8 +1,10 @@
 
-mklink /D C:\Users\darko\Documents\GitHub\zerog-shmupwarz\src\goop C:\msys64\mingw64\share\zerog\lib\goop 
-mklink /D C:\Users\darko\Documents\GitHub\zerog-shmupwarz\src\entitas C:\msys64\mingw64\share\zerog\lib\entitas
-mklink /D C:\Users\darko\Documents\GitHub\zerog-shmupwarz\src\sdx C:\msys64\mingw64\share\zerog\lib\sdx
-mklink /D C:\Users\darko\Documents\GitHub\zerog-shmupwarz\src\vapis C:\msys64\mingw64\share\zerog\lib\vapis
-mklink /D C:\Users\darko\Documents\GitHub\zerog-shmupwarz\src\vapis-em C:\msys64\mingw64\share\zerog\lib\vapis-em
-mklink C:\Users\darko\Documents\GitHub\zerog-shmupwarz\src\mt19937ar.c C:\msys64\mingw64\share\zerog\lib\mt19937ar.c
-mklink C:\Users\darko\Documents\GitHub\zerog-shmupwarz\src\mt19937ar.h C:\msys64\mingw64\share\zerog\lib\mt19937ar.h
+mklink /D C:\Users\darko\Documents\GitHub\zerog-shmupwarz\src\goop C:\Users\darko\Documents\GitHub\zerog\lib\goop 
+mklink /D C:\Users\darko\Documents\GitHub\zerog-shmupwarz\src\entitas C:\Users\darko\Documents\GitHub\zerog\lib\entitas
+mklink /D C:\Users\darko\Documents\GitHub\zerog-shmupwarz\src\sdx C:\Users\darko\Documents\GitHub\zerog\lib\sdx
+mklink /D C:\Users\darko\Documents\GitHub\zerog-shmupwarz\src\vapis C:\Users\darko\Documents\GitHub\zerog\lib\vapis
+mklink /D C:\Users\darko\Documents\GitHub\zerog-shmupwarz\src\vapis-em C:\Users\darko\Documents\GitHub\zerog\lib\vapis-em
+mklink C:\Users\darko\Documents\GitHub\zerog-shmupwarz\src\mt19937ar.c C:\Users\darko\Documents\GitHub\zerog\lib\mt19937ar.c
+mklink C:\Users\darko\Documents\GitHub\zerog-shmupwarz\src\mt19937ar.h C:\Users\darko\Documents\GitHub\zerog\lib\mt19937ar.h
+
+
