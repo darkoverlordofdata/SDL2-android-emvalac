021512713d181ae80396879e53cbb52dc500ac12
Bruce Davidson <darkoverlordofdata@gmail.com>
2017-10-16 22:29:05 -0700

    sync windows 10

--- symlinks.bat
+++ symlinks.bat
@@ -4,7 +4,6 @@ mklink /D C:\Users\darko\Documents\GitHub\zerog-shmupwarz\src\entitas C:\Users\d
 mklink /D C:\Users\darko\Documents\GitHub\zerog-shmupwarz\src\sdx C:\Users\darko\Documents\GitHub\zerog\lib\sdx
 mklink /D C:\Users\darko\Documents\GitHub\zerog-shmupwarz\src\vapis C:\Users\darko\Documents\GitHub\zerog\lib\vapis
 mklink /D C:\Users\darko\Documents\GitHub\zerog-shmupwarz\src\vapis-em C:\Users\darko\Documents\GitHub\zerog\lib\vapis-em
-mklink C:\Users\darko\Documents\GitHub\zerog-shmupwarz\src\mt19937ar.c C:\Users\darko\Documents\GitHub\zerog\lib\mt19937ar.c
-mklink C:\Users\darko\Documents\GitHub\zerog-shmupwarz\src\mt19937ar.h C:\Users\darko\Documents\GitHub\zerog\lib\mt19937ar.h
+mklink /D C:\Users\darko\Documents\GitHub\zerog-shmupwarz\src\mt19937 C:\Users\darko\Documents\GitHub\zerog\lib\mt19937
 
 
